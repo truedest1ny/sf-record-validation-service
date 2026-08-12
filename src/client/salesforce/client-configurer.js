ead00c295800a341b452d39a993e839f1cf16917
truedest1ny <oleg.kozel@succraft.com>
2026-08-12 15:35:18 +0300

    refactor(salesforce): inline token helper into OauthTokenManager and ClientConfigurer
    - Remove token-process-helper.js module
    - Move token processing logic into OauthTokenManager private methods;
    - Add private helper methods to ClientConfigurer for headers and interceptors.

--- src/client/salesforce/client-configurer.js
+++ src/client/salesforce/client-configurer.js
@@ -1,32 +1,44 @@
-import { getAuthorizationHeader } from "./token-process-helper.js";
-
 export default class ClientConfigurer {
-    oathManager = null;
+    tokenManager = null;
     apiClient = null;
 
-    constructor(apiClient = {},  oathManager = {}){
+    constructor(apiClient = {},  tokenManager = {}){
         this.apiClient = apiClient;
-        this.oathManager = oathManager;
+        this.tokenManager = tokenManager;
     }
 
-    async setTokenInRequestConfig(config){
-        const token = await this.oathManager.getToken();
-        config.headers.Authorization = getAuthorizationHeader(token);
+    setClientInterceptors(){
+        this.apiClient.interceptors.request.use(
+            async (config) => await this.#setTokenInRequestConfig(config)
+        );
+
+        this.apiClient.interceptors.response.use(
+            (response) => response,
+            async (error) => await this.#processUnauthorizedErrorResponse(error)
+        );
+}
+
+    async #setTokenInRequestConfig(config){
+        const token = await this.tokenManager.getToken();
+        config.headers['Authorization'] = this.#getAuthorizationHeader(token);
         console.log('Token set to header request');
         return config;
     }
 
-    async processUnauthorizedErrorResponse(errorResponse){
+    async #processUnauthorizedErrorResponse(errorResponse){
         const originalRequest = errorResponse.config;
     
-        if (errorResponse.response && errorResponse.response.status === 401 && !originalRequest._retry){
+        if (errorResponse.response && 
+            errorResponse.response.status === 401 && 
+            !originalRequest._retry){
+
             originalRequest._retry = true;
             
             try {
-                const newToken = await this.oathManager.refreshToken();
+                const newToken = await this.tokenManager.refreshToken();
 
                 console.log('Token refreshed')
-                originalRequest.headers['Authorization'] = getAuthorizationHeader(newToken);
+                originalRequest.headers['Authorization'] = this.#getAuthorizationHeader(newToken);
     
                 return await this.apiClient.request(originalRequest);
     
@@ -38,14 +50,7 @@ export default class ClientConfigurer {
         return Promise.reject(errorResponse);
     }
 
-    setClientInterceptors(){
-        this.apiClient.interceptors.request.use(
-            async (config) => await this.setTokenInRequestConfig(config)
-        );
-
-        this.apiClient.interceptors.response.use(
-            (response) => response,
-            async (error) => await this.processUnauthorizedErrorResponse(error)
-        );
+    #getAuthorizationHeader(token){
+        return `Bearer ${token}`;
     }
 }
--- src/client/salesforce/oauth-token-manager.js
+++ src/client/salesforce/oauth-token-manager.js
@@ -1,9 +1,9 @@
-import {fetchAccessToken} from "./token-process-helper.js";
-
 export default class OauthTokenManager {
 
     SF_TOKEN_ENDPOINT = '/services/oauth2/token';
     GRANT_TYPE = 'client_credentials';
+
+    ACCESS_TOKEN_KEY = 'access_token';
 
     httpClient = null;
 
@@ -15,18 +15,22 @@ export default class OauthTokenManager {
         this.connParams = connParams;
     }
 
-    setRequestParams(){
-        return new URLSearchParams({
-            grant_type : this.GRANT_TYPE,
-            client_id : this.connParams.clientId,
-            client_secret: this.connParams.clientSecret,
-        })
-    }
-
     async refreshToken(){
-        const params = this.setRequestParams();
+        const params = this.#setRequestParams();
 
-        this.currentToken = await fetchAccessToken(this.httpClient, this.SF_TOKEN_ENDPOINT, params);
+        try {
+
+            const response = await this.httpClient.post(this.SF_TOKEN_ENDPOINT, params)
+            this.currentToken = response.data[this.ACCESS_TOKEN_KEY];
+
+        } catch (error){
+
+            if (error.response){
+                console.log(error.response.data);
+            }
+            throw error;
+        }
+
         console.log('Token is successfully received');
         return this.currentToken;
     }
@@ -36,5 +40,13 @@ export default class OauthTokenManager {
             return this.currentToken;
         }
         return await this.refreshToken();
+    }
+    
+    #setRequestParams(){
+        return new URLSearchParams({
+            grant_type : this.GRANT_TYPE,
+            client_id : this.connParams.clientId,
+            client_secret: this.connParams.clientSecret,
+        })
     }
 }
