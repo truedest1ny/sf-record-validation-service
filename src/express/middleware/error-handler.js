2ef15dbcc62af570a19b8c2b553ac67777e1f97e
truedest1ny <oleg.kozel@succraft.com>
2026-08-20 17:44:54 +0300

    refactor(errorHandler) : added JSON syntax validation in globalErrorHandler middleware

--- src/express/middleware/error-handler.js
+++ src/express/middleware/error-handler.js
@@ -3,6 +3,12 @@ import AppError from "../../error/app-error.js";
 export function globalErrorHandler(error, req, res, next) {
     if (error instanceof AppError) {
         return res.status(error.statusCode).json(error.serialize());
+        
+    } else if (error instanceof SyntaxError) {
+        return res.status(400).json({
+            success : false,
+            message : 'Incorrect JSON syntax provided'
+        })
     }
 
     return res.status(500).json({
